26a9e353ef072ea3d9cba58a951109fb7c669b36
dmurdaugh <denzelmurdaugh@aol.com>
2017-03-30 15:43:47 -0400

    revert

--- app.js
+++ app.js
@@ -5,14 +5,11 @@
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
-// for more info, see: http://expressjs.co
-var metadataCount = 0;
-var id;
-var Client = require('ibmiotf');
+// for more info, see: http://expressjs.com
 var express = require('express');
-	request = require('request'),
+	request=require('request'),
 	
-	_= require('lodash');
+	_=require('lodash');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -27,71 +24,25 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// Iot Credentials 
-// --------------------
-var config = {
- "org" : "2c3wgz",
- "id" : "Denzel_IOT",
- "domain": "internetofthings.ibmcloud.com",
- "type" : "DC-iot",
- "auth-method": "token",
- "auth-token" : "7048419193"
- };
 
-var deviceClient = new Client.IotfDevice(config);
-deviceClient.connect();
-
-// Device Connection
-// -------------------
-
-deviceClient.on('connect', function () {
-  console.log("Device Connected");
-  
-});
-
-
-deviceClient.on("error", function (err) {
-    console.log("Error : **************************************************************************************************************************************************"+err);
-});
-
-// -------------------
-
-app.get('/process_get', function(req, res) {
-	// prepare output in json format
-	response = {
-		latitude:req.query.latitude,
-		longitude:req.query.longitude
+app.get('/process_get',function (req,res){
+		//prepare output in JSON format
+	response= {
+			latitude:req.query.latitude,
+			longitude:req.query.longitude
 	};
+	var callURL="https://f300582c-4b8d-4b90-acef-3c0545678094:29KwNW0xLT@twcservice.mybluemix.net/api/weather/v1/geocode/"+response.latitude+"/"+response.longitude+"/forecast/hourly/48hour.json?units=m&language=en-US"
 	
-	id=setInterval(function(){
-	  getWeather();
-	}, 150);
-})
-
-function getWeather(){
-	var callURL = "https://f300582c-4b8d-4b90-acef-3c0545678094:29KwNW0xLT@twcservice.mybluemix.net/api/weather/v1/geocode/"+response.latitude+"/"+response.longitude+"/forecast/hourly/48hour.json?units=m&language=en-US";
-	
-	request.get(callURL, {
-		json: true
+	request.get(callURL, { 
+		json:true
 	},
-	function (error, response, body) {
-		// Avoid cases where JSON property is undefined - application crashes
-		// Console Output is only for reference
-		// THE APPLICATION MUST BE STOPPED, CALLS WILL CONTINUE FOREVER!!!
-		if(body.hasOwnProperty('forecasts')) {
-		  console.log(metadataCount + " - The Parsed MetaData: ", body.forecasts[0]);
-		  deviceClient.publish("status","json", JSON.stringify(body.forecasts[0]));
-		  ++metadataCount;
-		  if (metadataCount == 3) {
-			clearInterval(id);
-		  }
-		}
+	function (error,response,body) {
+			console.log(body)
 	});
-	
-}
+})
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  console.log("server starting on ", appEnv.url);
+  console.log("server starting on " + appEnv.url);
 });
